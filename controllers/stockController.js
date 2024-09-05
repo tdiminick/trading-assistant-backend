09691c3fe753e573abbc42b2f8f6ca4348101a3c
Tyler Diminick <tdiminick@gmail.com>
2024-09-04 21:01:23 -0400

    package updates and stock price lookup updates

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -4,12 +4,14 @@ import logger from '../services/logger.js';
 const getStockPrice = async (req, res) => {
 	logger.info('get stock price: ', req.body, req.decoded);
 	const symbol = req.params.symbol;
-	const browser = await puppeteer.launch({ headless: true });
-	const page = await browser.newPage();
 
-	await page.goto(`https://finance.yahoo.com/quote/${symbol}`);
-
+	let browser;
 	try {
+		browser = await puppeteer.launch({ headless: true });
+		const page = await browser.newPage();
+
+		await page.goto(`https://finance.yahoo.com/quote/${symbol}`);
+
 		// evaluate and find the proper element
 		const result = await page.evaluate(() => {
 			return document.querySelector('fin-streamer.price')?.textContent || document.querySelector('fin-streamer.livePrice')?.textContent;
